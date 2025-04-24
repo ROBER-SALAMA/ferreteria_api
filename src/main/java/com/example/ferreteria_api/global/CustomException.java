8e800aea82686b50a8c8f4c1c77794a6da07ffb1
ROBER-SALAMA <robeetosalama12@gmail.com>
2025-04-24 12:31:22 -0600

    agregue carpeta global

---
+++ src/main/java/com/example/ferreteria_api/global/CustomException.java
@@ -0,0 +1,16 @@
+package com.example.ferreteria_api.global;
+import org.springframework.http.HttpStatus;
+
+public class CustomException extends RuntimeException{
+
+    private final HttpStatus httpStatus;
+
+    public CustomException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+}
