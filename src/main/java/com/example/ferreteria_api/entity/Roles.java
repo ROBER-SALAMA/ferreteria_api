c65bca533fc22eaa36668c632e23afab24bf9f58
ROBER-SALAMA <robeetosalama12@gmail.com>
2025-04-23 21:55:47 -0600

    primer commit

---
+++ src/main/java/com/example/ferreteria_api/entity/Roles.java
@@ -0,0 +1,17 @@
+package com.example.ferreteria_api.entity;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class Roles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(columnDefinition = "boolean default true")
+    private boolean isActive = true;
+}
